2b8c3170b737333278d07e0097e85413e3075ad8
Dulatto <dulat.omarov@gmail.com>
2022-03-15 23:19:36 -0400

    appbar

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+
+function ElevationScroll(props) {
+    const { children } = props;
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
 
 function Header(props) {
     return (
-        <AppBar position='fixed'>
-            <Toolbar>JS Development</Toolbar>
-        </AppBar>
+        <ElevationScroll>
+            <AppBar position='fixed'>
+                <Toolbar>JS Development</Toolbar>
+            </AppBar>
+        </ElevationScroll>
     )
 }
 
