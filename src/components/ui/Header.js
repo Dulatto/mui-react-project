96e1d5b43bd48ae799abaea627e6d789594e33a7
Dulatto <dulat.omarov@gmail.com>
2022-03-15 23:02:51 -0400

    appbar

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+function Header(props) {
+    return (
+        <AppBar>
+            <Toolbar>JS Development</Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header
