c78eb5d58da4419b3bdd813ae09b6e9e594a68cc
Dulatto <dulat.omarov@gmail.com>
2022-03-16 23:30:24 -0400

    theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import Header from "./ui/Header";
+import theme from './ui/Theme'
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header />
       Hello
-    </div>
+    </ThemeProvider>
   );
 }
 
